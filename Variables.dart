f11a3d56f6ef889555e6be85d2a501e8a3fca7a5
Edivaldo Gomes <edslavianov@gmail.com>
2023-03-01 02:20:42 +0100

    Variables in dart

---
+++ Variables.dart
@@ -0,0 +1,30 @@
+void main() {
+  //Variables
+
+  //Strings
+  //var name = "John";
+  String name = "John";
+
+  //Integer
+  //var x = 41;
+  int x = 41;
+
+  print(name);
+  print(x);
+
+  // Boolean
+  bool b = true;
+
+  //Dinamic
+  dynamic firsName = "Tim";
+
+  // Const and final
+  const String fullName = "Edivaldo Gomes";
+  final String nickName = "Edi";
+
+  //declaring and not using
+  var myName;
+  print(myName);
+  myName = "Edivaldo";
+  print(myName);
+}
