0c71ebefd32881458fa97c6b4774936e388033fe
Edivaldo Gomes <edslavianov@gmail.com>
2023-03-01 02:35:29 +0100

    Data types

---
+++ Datatypes.dart
@@ -0,0 +1,25 @@
+void main() {
+  //Data Types
+//Numbers, Strings, Booleans, Lists, Maps, Dynamic
+
+// Strings
+  String firstName = "Edivaldo";
+  String secondName = "Gomes";
+  print("String: $firstName $secondName");
+
+// Integers
+  int age = 25;
+  print("Integer: $age");
+
+// Doubles
+  double otherAge = 26.10;
+  print("Double: $otherAge");
+
+// Booleans
+  bool status = false;
+  print("Boolean: $status");
+
+// Dynamic
+  dynamic fullName = "Edivaldo Gomes";
+  print("Dynamic: $fullName");
+}
