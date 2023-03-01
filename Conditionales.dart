6cb76de7873cbf5b6cf84e6a69147d87193135b8
Edivaldo Gomes <edslavianov@gmail.com>
2023-03-01 03:21:18 +0100

    Conditionales in dart

---
+++ Conditionales.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  //Logic -if, if/Else,, if else if
+  var num = 6;
+  if (num == 5) {
+    print('The number is 5!');
+  } else if (num == 3) {
+    print('The number is 3!');
+  } else {
+    print('The number is not 3 or 5 is $num');
+  }
+}
